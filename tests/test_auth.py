81ffaf171650d781b1926abc388d2c1241b97908
data-day-life <mtvatemybrains@gmail.com>
2020-04-27 17:13:10 -0400

    Tweak after refactoring Auth class; all tests passed.

--- tests/test_auth.py
+++ tests/test_auth.py
@@ -5,8 +5,7 @@ import unittest
 
 class TestAuth(unittest.TestCase):
     def setUp(self):
-        auth_args = {'client_id': settings.TWITCH_CLIENT_ID, 'client_secret': settings.TWITCH_CLIENT_SECRET}
-        self.test_auth = Auth(**auth_args)
+        self.test_auth = Auth()
 
     def test_auth_has_cid(self):
         self.assertEqual(self.test_auth.client_id, settings.TWITCH_CLIENT_ID)
@@ -15,23 +14,23 @@ class TestAuth(unittest.TestCase):
         self.assertEqual(self.test_auth.client_secret, settings.TWITCH_CLIENT_SECRET)
 
     def test_auth_has_auth_tok(self):
-        self.assertTrue(hasattr(self.test_auth, 'auth_tok'))
-        self.assertFalse(self.test_auth.auth_tok == "")
-        self.assertTrue('access_token' in self.test_auth.auth_tok)
-        self.assertTrue(len(self.test_auth.auth_tok['access_token']) > 0)
-        self.assertFalse(self.test_auth.auth_tok['access_token'] == "")
+        self.assertTrue(hasattr(self.test_auth, 'auth_token'))
+        self.assertFalse(self.test_auth.auth_token == "")
+        self.assertTrue('access_token' in self.test_auth.auth_token)
+        self.assertTrue(len(self.test_auth.auth_token['access_token']) > 0)
+        self.assertFalse(self.test_auth.auth_token['access_token'] == "")
 
     def test_auth_has_bear_tok(self):
-        self.assertTrue(hasattr(self.test_auth, 'bear_tok'))
-        self.assertFalse(self.test_auth.bear_tok == "")
+        self.assertTrue(hasattr(self.test_auth, 'bear_token'))
+        self.assertFalse(self.test_auth.bear_token == "")
         # Check Authorization field
-        self.assertTrue('Authorization' in self.test_auth.bear_tok)
-        self.assertTrue(len(self.test_auth.bear_tok['Authorization']) > 0)
-        self.assertFalse(self.test_auth.bear_tok['Authorization'] == "")
+        self.assertTrue('Authorization' in self.test_auth.bear_token)
+        self.assertTrue(len(self.test_auth.bear_token['Authorization']) > 0)
+        self.assertFalse(self.test_auth.bear_token['Authorization'] == "")
         # Check Client_ID field
-        self.assertTrue('Client_ID' in self.test_auth.bear_tok)
-        self.assertTrue(len(self.test_auth.bear_tok['Client_ID']) > 0)
-        self.assertFalse(self.test_auth.bear_tok['Client_ID'] == "")
+        self.assertTrue('Client_ID' in self.test_auth.bear_token)
+        self.assertTrue(len(self.test_auth.bear_token['Client_ID']) > 0)
+        self.assertFalse(self.test_auth.bear_token['Client_ID'] == "")
 
 
 if __name__ == '__main__':
