f3ca7887f5f703d2bbaed3eec182190fdbae9acd
data-day-life <mtvatemybrains@gmail.com>
2020-04-27 17:12:23 -0400

    Renamed bear_tok attribute to bear_token.

--- app/auth.py
+++ app/auth.py
@@ -13,7 +13,7 @@ class Auth:
         # Initialize class attributes
         self.client_id = settings.TWITCH_CLIENT_ID
         self.client_secret = settings.TWITCH_CLIENT_SECRET
-        self.auth_tok, self.bear_tok = self.get_token()
+        self.auth_token, self.bear_token = self.get_token()
         self.fetched_at = None
         self.expires_at = None
 
@@ -25,19 +25,19 @@ class Auth:
             'grant_type':     'client_credentials'
             }
         with requests.post(oauth_url, data=auth_params) as req:
-            self.auth_tok = req.json()
-            self.bear_tok = {
-                'Authorization':  'Bearer ' + self.auth_tok['access_token'],
+            self.auth_token = req.json()
+            self.bear_token = {
+                'Authorization':  'Bearer ' + self.auth_token['access_token'],
                 'Client_ID':      self.client_id
                 }
         # Capturing & String-Formatting Token Lifetime Information
             self.fetched_at = parser.parse(req.headers['date']).strftime(self.twitch_time_fmt)
-            expires_at = parser.parse(self.fetched_at) + timedelta(seconds=self.auth_tok['expires_in'])
+            expires_at = parser.parse(self.fetched_at) + timedelta(seconds=self.auth_token['expires_in'])
             # Maintain a 3 day buffer between End-of-Life according to Twitch vs End-of-Life known to this app
             expires_at -= timedelta(days=3)
             self.expires_at = expires_at.strftime(self.twitch_time_fmt)
 
-        return self.auth_tok, self.bear_tok
+        return self.auth_token, self.bear_token
 
     def not_expired(self) -> bool:
         return datetime.utcnow().astimezone(tz=pytz.utc) < parser.parse(self.expires_at)
@@ -48,7 +48,7 @@ class Auth:
         # If token has not exceeded lifetime, validate with Twitch
         if self.not_expired():
             oauth_url = 'https://id.twitch.tv/oauth2/validate'
-            auth_header = {'Authorization': 'OAuth {}'.format(self.auth_tok['access_token'])}
+            auth_header = {'Authorization': 'OAuth {}'.format(self.auth_token['access_token'])}
             with requests.get(oauth_url, headers=auth_header) as req:
                 # The OAuth token is valid if json response from Twitch contains 'client_id'
                 valid = 'client_id' in req.json()
