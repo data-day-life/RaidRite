83f4ddc3c2ead7157741bb765e41095b6c07d4ae
data-day-life <mtvatemybrains@gmail.com>
2020-04-28 18:06:58 -0400

    Added route for suggestions.

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from app.controllers import blueprints
 from flask import render_template
+from app.lean_twitch_client import TwitchClient, get_userinfo
 
 app = Flask(__name__, template_folder='../templates')
 
@@ -9,6 +10,15 @@ for blueprint in blueprints:
 
 @app.route('/')
 @app.route('/index')
-
 def index():
     return render_template('index.html')
+
+@app.route('/user/<username>')
+def suggestions(username):
+    userinfo = get_userinfo(username)
+    if userinfo and 'uid' in userinfo:
+        suggestions = TwitchClient.get_similar_streams(userinfo['uid'])
+    else:
+        suggestions = None
+
+    return suggestions
