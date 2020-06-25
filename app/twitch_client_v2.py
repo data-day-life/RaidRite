44f5f31af3c08584c566bfaadb7557467acdbd0d
data-day-life <mtvatemybrains@gmail.com>
2020-06-25 02:25:45 -0400

    added get_uids()

--- app/twitch_client_v2.py
+++ app/twitch_client_v2.py
@@ -1,14 +1,39 @@
 import asyncio
-from twitchio import client
+from app.settings import TWITCH_CLIENT_ID, TWITCH_CLIENT_SECRET
+from twitchio.client import Client
+from time import perf_counter
+from typing import Union
 
-from app.auth import Auth
-from app.settings import TWITCH_CLIENT_ID
+
+class TC(Client):
+    
+    def __init__(self):
+        super().__init__(client_id=TWITCH_CLIENT_ID, client_secret=TWITCH_CLIENT_SECRET)
+        self.http = self.http
+
+    async def close(self):
+        await self.http._session.close()
+
+    async def get_fols(self, user_id, limit=None):
+        params = [('to_id', user_id)]
+        return await self.http.request('GET', '/users/follows', params=params, limit=limit, count=False)
+
+    async def get_users(self, *users: Union[str, int]):
+        users = await self.http.get_users(*users)
+        return await users
+
+    async def get_uids(self, *usr_name: tuple):
+        uids = await self.http.get_users(*usr_name)
+        return [int(user['id']) for user in uids]
+
 
 async def main():
-    twitch_client = client.Client(client_id=TWITCH_CLIENT_ID)
-    print(await twitch_client.get_users("stroopc"))
-    print('')
-    twitch_client.http._session.close()
+    tc = TC()
+    test_names = ['stroopc', 'jitterted', 'strager']
+    uids = await tc.get_uids(*test_names)
+    print(uids)
+
+    await tc.close()
 
 
 if __name__ == "__main__":
