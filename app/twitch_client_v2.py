d107c48e47f7f51446f25e90c9797632deb2cb58
data-day-life <mtvatemybrains@gmail.com>
2020-07-03 15:56:10 -0400

    Added functionality to get complete follower reply ('data', 'total', 'cursor')

--- app/twitch_client_v2.py
+++ app/twitch_client_v2.py
@@ -5,7 +5,7 @@ from collections import Counter
 from time import perf_counter
 
 
-class TC(Client):
+class TwitchClient(Client):
     
     def __init__(self, loop=None):
         self.loop = loop or asyncio.get_event_loop()
@@ -17,20 +17,32 @@ class TC(Client):
         await self.http._session.close()
 
 
-    async def _get_total(self, user_id, params=None, limit=None):
-        if not params:
-            params = [('to_id', user_id)]
-        return await self.http.request('GET', '/users/follows', params=params, limit=limit, count=True)
-
-
     async def get_total_followers(self, user_id):
         params = [('to_id', user_id)]
-        return await self._get_total(user_id, params)
-
+        return await self.http.request('GET', '/users/follows', params=params, count=True)
 
     async def get_total_followings(self, user_id):
         params = [('from_id', user_id)]
-        return await self._get_total(user_id, params)
+        return await self.http.request('GET', '/users/follows', params=params, count=True)
+
+
+    async def get_n_followers(self, user_id, n_folls=300, wanted_full=False, params=None):
+        params = params or []
+        params.extend([('to_id', user_id)])
+        return await self.http.request('GET', '/users/follows', params=params, limit=n_folls, full_reply=wanted_full)
+
+    async def get_n_followings(self, user_id, n_folls=100,  wanted_full=False, params=None):
+        params = params or []
+        params.extend([('from_id', user_id)])
+        return await self.http.request('GET', '/users/follows', params=params, limit=n_folls, full_reply=wanted_full)
+
+
+    async def get_full_n_followers(self, user_id, n_folls=300, params=None):
+        return await self.get_n_followers(user_id, n_folls, wanted_full=True, params=params)
+
+    async def get_full_n_followings(self, user_id, n_folls=100, params=None):
+        return await self.get_n_followings(user_id, n_folls, wanted_full=True, params=params)
+
 
 
     # async def get_users(self, *users: Union[str, int]):
@@ -46,33 +58,42 @@ class TC(Client):
     async def get_uids(self, *user_names: tuple):
         return [user.id for user in await self.get_users(*user_names)]
 
+
+    async def get_uid(self, user_name):
+        user = (await self.get_users(user_name))[0]
+        return user.id
+
     # async def get_follower_count(self, user_id):
 
 
-async def main():
-    tc = TC()
-    test_names = ['stroopc', 'jitterted', 'strager']
-    test_names = ['stroopc']
-    uids = await tc.get_uids(*test_names)
-    print(uids)
+async def main(name_list):
+    tc = TwitchClient()
+
+    # User IDs
+    uids = await tc.get_uids(*name_list)
+    print(f'User IDs: \n {uids}')
 
     # Followers
     fols = await tc.get_followers(uids[0])
     print(f'Followers: \n {fols}')
 
     # Followings
-    fols = await tc.get_following(uids[0])
-    print(f'Followings: \n {fols}')
+    # fols = await tc.get_following(uids[0])
+    # print(f'Followings: \n {fols}')
 
     # Chatters
-    chatters = await tc.get_chatters(*test_names)
-    print(f'Chatters: \n {chatters}')
+    # chatters = await tc.get_chatters(*name_list)
+    # print(f'Chatters: \n {chatters}')
 
+    # Cleanup open session sockets and event loops
     await tc.close()
 
 
 if __name__ == "__main__":
+    # test_names = ['stroopc', 'jitterted', 'strager']
+    test_names = ['stroopc']
+
     start_time = perf_counter()
-    asyncio.run(main())
+    asyncio.run(main(test_names))
     print(f'Run time: {round(perf_counter() - start_time, 3)} sec')
 
