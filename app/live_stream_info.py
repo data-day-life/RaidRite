4c8e1f51e055bb14e834adb51863fefaab548237
data-day-life <mtvatemybrains@gmail.com>
2020-07-14 19:20:31 -0400

    Gets live stream info for a given uid list of (mutual) followings.  Created for use with pipeline.

--- app/live_stream_info.py
+++ app/live_stream_info.py
@@ -1,34 +1,56 @@
 import asyncio
 from app.twitch_client_v2 import TwitchClient
-from collections import Counter
+from app.streamer import Streamer
+from app.follower_network import FollowNet
+from time import perf_counter
+
 
 
 class LiveStreamInfo:
     MAX_BATCH_SZ = 100
 
-    def __init__(self, tc: TwitchClient=None):
+    def __init__(self, tc: TwitchClient = None):
         self.tc = tc or TwitchClient()
         self.live_streams = list()
         self.checked_live_uids = set()
         self.unchecked_live_uids = set()
         self.num_live_stream_calls_to_twitch = 0
-        self.followings_counter = Counter()
+        self.last_mutual_followings = None
 
-    # TODO: Address Followings Counter
-    async def check_live(self, min_mutual=2):
-        mutual_followings = {uid for uid, count in self.followings_counter.items() if count >= min_mutual}
+
+    def __str__(self, result=''):
+        result += f'Calls to Twitch:  {self.num_live_stream_calls_to_twitch}\n'
+        result += f' Live Streams (len): {len(self.live_streams)}\n'
+        result += f'  Unchecked Live UIDs (len):  {len(self.unchecked_live_uids)}\n'
+        result += f'    Checked Live UIDs (len):  {len(self.checked_live_uids)}\n'
+        result += '-.-'*10+'\n'
+        result += f' Live Streams:  {self.live_streams}\n'
+        result += f' Unchecked Uids:  {self.unchecked_live_uids}\n'
+        result += f' Checked Uids:  {self.checked_live_uids}'
+        result += '-.-' * 10 + '\n'
+        result += f'Last Mutual Followings (len): {len(self.last_mutual_followings)}\n'
+        result += f'Last Mutual Followings: {self.last_mutual_followings}'
+
+        return result
+
+
+    async def consume_mutual_followings(self, q_in: asyncio.Queue, q_out: asyncio.Queue = None):
+        while True:
+            mutual_followings = await q_in.get()
+            self.last_mutual_followings = mutual_followings
+            await self.check_live(mutual_followings)
+
+            if q_out:
+                pass
+            q_in.task_done()
+
+
+    async def check_live(self, mutual_followings):
         if mutual_followings and len(mutual_followings) >= self.MAX_BATCH_SZ:
             self.unchecked_live_uids = mutual_followings - self.checked_live_uids
-            # TODO: maybe use 'try' and 'finally' here instead?
 
             if len(self.unchecked_live_uids) >= self.MAX_BATCH_SZ:
                 await self.update_live_streams()
-            # Get remaining candidates after all batches of 100 were collected (unchecked + checked == mutual)
-            # elif self.checked_live_uids:
-            #     if self.unchecked_live_uids.union(self.checked_live_uids) == mutual_followings:
-            #         print(f'Length of unchecked uids: {len(self.unchecked_live_uids)}')
-            #
-            # ^^^^^^^^^ DON'T FORGET TO DO THIS STEP IN THIS CLASS
 
 
     async def update_live_streams(self):
@@ -39,9 +61,25 @@ class LiveStreamInfo:
 
         self.checked_live_uids.update(candidates)
         live_candidates = await self.tc.get_streams(channels=candidates)
-        self.num_live_stream_calls_to_twitch += 1
         self.live_streams.extend(live_candidates)
+        self.num_live_stream_calls_to_twitch += 1
 
+
+async def run_queue(tc: TwitchClient, streamer: Streamer, folnet: FollowNet, ls: LiveStreamInfo, n_consumers=50):
+    q_followers = asyncio.Queue()
+    q_followings = asyncio.Queue()
+    await streamer.produce_follower_samples(tc, q_out=q_followers, print_status=True)
+
+    consume_followings = [asyncio.create_task(
+        folnet.consume_follower_samples(q_in=q_followers, q_out=q_followings)) for _ in range(n_consumers)]
+    consume_livestreams = asyncio.create_task(ls.consume_mutual_followings(q_followings))
+
+    await q_followers.join()
+    await q_followings.join()
+
+    for c in consume_followings:
+        c.cancel()
+    consume_livestreams.cancel()
 
 
 
@@ -49,49 +87,29 @@ async def main():
     some_name = 'emilybarkiss'
     sample_sz = 300
     n_consumers = 100
-    n_consumers = 2
-    # await run_format(some_name, sample_sz, n_consumers)
-
+    await run_format(some_name, sample_sz, n_consumers)
 
 
 async def run_format(some_name, sample_sz, n_consumers):
-    tc = TwitchClient()
-    streamer_id = await tc.get_uid(some_name)
-    folnet = FollowNetLiveStreams(streamer_id, tc, sample_sz=sample_sz, n_cons=n_consumers)
-
     from app.colors import Col
-    print(f'{Col.bold}{Col.yellow}\t<<<<< {some_name}  |  n={sample_sz} >>>>>{Col.end}')
     t = perf_counter()
 
-    # follower_ids = await folnet.produce_follower_samples(print_status=True)
-    # print(f'Follower ID List:\n {follower_ids}')
+    tc = TwitchClient()
+    streamer = Streamer(name=some_name, sample_sz=sample_sz)
+    await streamer(tc)
+    folnet = FollowNet(tc, streamer.streamer_uid)
+    ls = LiveStreamInfo(tc)
 
-    await folnet.run_queue()
-    # print(folnet.followings_counter)
-    # print(f'Counter length: {len(folnet.followings_counter)}')
+    print(f'{Col.bold}{Col.yellow}\t<<<<< {some_name}  |  n={sample_sz} >>>>>{Col.end}')
+    print(f'\t\t{Col.yellow}uid: {streamer.streamer_uid}{Col.end}')
+    print(f'\t{Col.magenta}N consumers: {n_consumers}{Col.end}')
 
+    await run_queue(tc, streamer, folnet, ls, n_consumers)
+
+    print(ls)
     print(f'{Col.cyan}⏲ Total Time: {round(perf_counter() - t, 3)} sec {Col.end}')
-    print(f'\t{Col.magenta}N consumers: {n_consumers}\n')
 
-    print(f'{Col.green}Suggestions (sz={len(folnet.followings_counter)}){Col.end}')
-    print(folnet.followings_counter)
-
-    print(f'{Col.green}Live Stream Suggestions (sz={len(folnet.live_streams)}){Col.end}')
-    print(folnet.live_streams)
-
-    print(f'{Col.green}Num Unchecked for live status: (sz={len(folnet.unchecked_live_uids)}){Col.end}')
-    print(folnet.live_streams)
-
-    # trimmed_suggs = {uid: count for uid, count in folnet.followings_counter.items() if count >= 2}
-    # print(f'{Col.yellow}Trimmed Suggestions (sz={len(trimmed_suggs)}){Col.end}')
-    # print(trimmed_suggs)
-    #
-    # mutual =  {uid for uid, count in folnet.followings_counter.items() if count >= 2}
-    # print(f'mutual: {mutual}')
-    # print(f'len(mutual): {len(mutual)}')
-
-
-    await folnet.tc.close()
+    await tc.close()
 
 
 if __name__ == "__main__":
