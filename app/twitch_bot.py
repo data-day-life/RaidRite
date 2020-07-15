c5c2fc219e2d212c0ddb4455122bb70c4cf5d8d9
data-day-life <mtvatemybrains@gmail.com>
2020-07-15 16:41:17 -0400

    Added webhook usage example.

--- app/twitch_bot.py
+++ app/twitch_bot.py
@@ -2,19 +2,30 @@
 
 from app.settings import *
 from twitchio.ext import commands
+from twitchio.enums import WebhookMode
+from twitchio.webhook import StreamChanged
 
 from time import perf_counter
 
 
+# Added auth to http.py for twitchio master; set headers = self.bear_token after headers set to client_id
+
 class Bot(commands.Bot):
 
     def __init__(self):
-        super().__init__(irc_token=TMI_TOKEN, client_id=TWITCH_CLIENT_ID,
-                        nick=BOT_NICK, prefix=BOT_PREFIX, initial_channels=CHANNELS )
+        super().__init__(irc_token=TMI_TOKEN,
+                         client_id=TWITCH_CLIENT_ID,
+                         client_secret=TWITCH_CLIENT_SECRET,
+                         nick=BOT_NICK,
+                         prefix=BOT_PREFIX,
+                         initial_channels=CHANNELS
+                         )
 
     # Events don't need decorators when subclassed
     async def event_ready(self):
         print(f'Ready | {self.nick}')
+        total = await self.get_followers(106071345, count=True)
+        print(f'Total Followers: {total}')
         start_time = perf_counter()
         result = await self.get_followers(106071345)
         print(f'length: {len(result)}, \nresult: {result}')
@@ -29,6 +40,9 @@ class Bot(commands.Bot):
     async def my_command(self, ctx):
         await ctx.send(f'Hello {ctx.author.name}!')
 
+    async def webhook_sub(self):
+        await self.modify_webhook_subscription(callback="https://mattboy.live/api/twitch", mode=WebhookMode.subscribe,
+                                               topic=StreamChanged(12492867))
 
     async def foo(self):
         return await self.get_followers(106071345)
