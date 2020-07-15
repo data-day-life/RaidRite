1055e2576b5acb0b2223d04f145f38a18a706d5c
data-day-life <mtvatemybrains@gmail.com>
2020-07-15 17:26:07 -0400

    Modified 'blue' -> 'orange' to match console.

--- app/colors.py
+++ app/colors.py
@@ -2,7 +2,7 @@ class Col():
     red = u'\u001b[31;1m'
     green = u'\u001b[32;1m'
     yellow = u'\u001b[33;1m'
-    blue = u'\u001b[34;1m'
+    orange = u'\u001b[34;1m'
     magenta = u'\u001b[35;1m'
     cyan = u'\u001b[36;1m'
     white = u'\u001b[37;1m'
