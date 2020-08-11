3f2a5074b5ba1bb40c93febf863e8bf53f630d48
data-day-life <mtvatemybrains@gmail.com>
2020-08-11 12:03:11 -0400

    Constructor params now accept objects instead of primitives; added class method to construct from primitives.

--- app/twitch_rec/pipeline_builder.py
+++ app/twitch_rec/pipeline_builder.py
@@ -1,29 +1,31 @@
 import asyncio
 from app.twitch_rec.twitch_client import TwitchClient
 from app.twitch_rec.streamer import StreamerPipe
-from app.twitch_rec.follower_network import FollowNetPipe
-from app.twitch_rec.live_stream_info import LiveStreamPipe
+from app.twitch_rec.follower_network import FollowNetPipe, FollowerNetwork
+from app.twitch_rec.live_stream_info import LiveStreamPipe, LiveStreams
 
 
-class FollowNetPipeline:
-    sample_sz:      int = 300
-    max_followings: int = 150
-    min_mutual:     int = 3
+class PipelineBuilder:
 
-    def __init__(self, streamer_name: str, sample_sz: int = 300, max_followings: int = 150, min_mutual: int = 3) -> None:
-        self.sample_sz = sample_sz
-        self.max_followings = max_followings
-        self.min_mutual = min_mutual
-        self.streamer_pipe = StreamerPipe(streamer_name, sample_sz=sample_sz)
-        self.folnet_pipe = FollowNetPipe(self.streamer_pipe.uid, max_followings, min_mutual)
-        self.live_stream_pipe = LiveStreamPipe()
+    def __init__(self, streamer: StreamerPipe, folnet: FollowerNetwork, live_streams: LiveStreams) -> None:
+        self.streamer_pipe = streamer
+        self.folnet_pipe = FollowNetPipe(folnet)
+        self.live_stream_pipe = LiveStreamPipe(live_streams)
+
+
+    @classmethod
+    def from_name(cls, streamer_name: str, sample_sz: int = 300):
+        streamer = StreamerPipe(name=streamer_name, sample_sz=sample_sz)
+        folnet = FollowerNetwork(streamer.uid)
+        live_streams = LiveStreams()
+
+        return cls(streamer, folnet, live_streams)
 
 
     async def __call__(self, tc: TwitchClient, n_consumers: int):
         q_foll_ids = asyncio.Queue()
         q_followings = asyncio.Queue()
         q_live_uids = asyncio.Queue()
-
 
         t_prod = asyncio.create_task(self.streamer_pipe(tc, q_out=q_foll_ids))
         t_followings = [asyncio.create_task(
@@ -59,13 +61,16 @@ async def main():
     sample_sz = 350
     n_consumers = 100
 
-    pipe = FollowNetPipeline(some_name, sample_sz)
+    streamer = StreamerPipe('emilybarkiss', sample_sz=sample_sz)
+    folnet = FollowerNetwork(streamer.uid)
+    live_streams = LiveStreams()
+    pipeline = PipelineBuilder(streamer, folnet, live_streams)
     async with TwitchClient() as tc:
-        await pipe(tc, n_consumers)
+        await pipeline(tc, n_consumers)
 
-    print(pipe.streamer_pipe)
-    print(pipe.folnet_pipe)
-    print(pipe.live_stream_pipe)
+    print(pipeline.streamer_pipe)
+    pipeline.folnet_pipe.display
+    pipeline.live_stream_pipe.display
 
     print(f'{Col.magenta}🟊 N consumers: {n_consumers} {Col.end}')
     print(f'{Col.cyan}⏲ Total Time: {round(perf_counter() - t, 3)} sec {Col.end}')
